1622cfc46e5ea5eae0a4a58e21d7dfc14cd79a98
Ben Magee <ben@bmagee.com>
2020-10-25 14:38:02 +0000

    Whitespace fix.

--- src/config/laravel_opentelemetry.php
+++ src/config/laravel_opentelemetry.php
@@ -2,8 +2,6 @@
 
 return [
     'enable' => true,
-
-    'zipkin_endpoint' => 'http://localhost:9411/api/v2/spans',
-
-    'service_name' => 'laravel-otel'
+    'service_name' => 'laravel-otel',
+    'zipkin_endpoint' => 'http://localhost:9411/api/v2/spans'
 ];
