8852ae3ebb860df50b03a3598711fb8bc6b04149
Ben Magee <ben@bmagee.com>
2020-10-25 15:29:27 +0000

    Set span status depending on HTTP response code.

--- src/Middleware/Trace.php
+++ src/Middleware/Trace.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use OpenTelemetry\Trace\Span;
+use OpenTelemetry\Trace\SpanStatus;
 use OpenTelemetry\Trace\Tracer;
 
 /**
@@ -35,12 +36,39 @@ class Trace
         $span = $this->tracer->startAndActivateSpan('http_'.strtolower($request->method()));
         $response = $next($request);
 
+        $this->setSpanStatus($span, $response->status());
         $this->addConfiguredTags($span, $request, $response);
         $span->setAttribute('response.status', $response->status());
         
         $this->tracer->endActiveSpan();
 
         return $response;
+    }
+
+    private function setSpanStatus(Span $span, int $httpStatusCode)
+    {
+        switch($httpStatusCode) {
+            case 400:
+                $span->setSpanStatus(SpanStatus::FAILED_PRECONDITION, SpanStatus::DESCRIPTION[SpanStatus::FAILED_PRECONDITION]);
+                return;
+            case 401:
+                $span->setSpanStatus(SpanStatus::UNAUTHENTICATED, SpanStatus::DESCRIPTION[SpanStatus::UNAUTHENTICATED]);
+                return;
+            case 403:
+                $span->setSpanStatus(SpanStatus::PERMISSION_DENIED, SpanStatus::DESCRIPTION[SpanStatus::PERMISSION_DENIED]);
+                return;
+            case 404:
+                $span->setSpanStatus(SpanStatus::NOT_FOUND, SpanStatus::DESCRIPTION[SpanStatus::NOT_FOUND]);
+                return;
+        }
+
+        if($httpStatusCode >= 500 && $httpStatusCode < 600) {
+            $span->setSpanStatus(SpanStatus::INTERNAL, SpanStatus::DESCRIPTION[SpanStatus::INTERNAL]);
+        }
+
+        if($httpStatusCode >= 200 && $httpStatusCode <= 300) {
+            $span->setSpanStatus(SpanStatus::OK, SpanStatus::DESCRIPTION[SpanStatus::OK]);
+        }
     }
 
     private function addConfiguredTags(Span $span, Request $request, $response)
